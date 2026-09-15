34f75bc679718c2a960445bc556ce0f3f2289828
afriezza <wongjen07@gmail.com>
2026-09-15 21:23:56 +0700

    retire dynamic -> 3 funcs

--- api/r5/[slot].js
+++
@@ -1,7 +0,0 @@
-import h from "../gen.js";
-export default function handler(req,res){
-  const slot=(req.query&&req.query.slot)||"1";
-  req.query={...(req.query||{}),slot};
-  res.setHeader("X-Api-Group","r5");
-  return h(req,res);
-}
