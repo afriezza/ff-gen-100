bd2b24ea19cb8322754cb01d237153cad3639364
afriezza <wongjen07@gmail.com>
2026-09-15 20:53:47 +0700

    group r1 250 slots

---
+++ api/r1/[slot].js
@@ -0,0 +1,7 @@
+import h from "../../gen.js";
+export default function handler(req,res){
+  const slot=(req.query&&req.query.slot)||"1";
+  req.query={...(req.query||{}),slot};
+  res.setHeader("X-Api-Group","r1");
+  return h(req,res);
+}
